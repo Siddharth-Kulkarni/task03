ec66001f3b51ea141e2f66b8c4ac6bf83596921f
Siddharth-Kulkarni <120244251+Siddharth-Kulkarni@users.noreply.github.com>
2023-10-23 14:16:48 +0530

    Added files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
+function checkPasswordStrength(password) {
+   
+    var password = document.getElementById("password").value;
+
+       let passwordStrengthDiv = document.getElementById('password-strength');
+         passwordStrengthDiv.className = '';
+    
+         if (password.length < 6) {
+             passwordStrengthDiv.className = 'weak';
+             passwordStrengthDiv.innerText = 'Weak';
+             passwordStrengthDiv.style.color="red";
+         } else if (password.length >= 6 && password.length <= 8) {
+             passwordStrengthDiv.className = 'medium';
+             passwordStrengthDiv.innerText = 'Medium';
+             passwordStrengthDiv.style.color = "orange";
+         } else {
+             passwordStrengthDiv.className = 'strong';
+             passwordStrengthDiv.innerText = 'Strong';
+             passwordStrengthDiv.style.color = "green";
+    
+         }
+     }
+
+function validatePassword() {
+    const password = document.getElementById("password").value;
+    const confirm = document.getElementById("confirm-password").value;
+    const messageSpan = document.getElementById("message");
+
+    if (password === confirm) {
+        messageSpan.innerHTML = "Passwords match!";
+        messageSpan.style.color = "green";
+    } else {
+        messageSpan.innerHTML = "Passwords do not match!";
+        messageSpan.style.color = "red";
+    }
+}
+function openWebsite(){
+    window.open("https://vaultofcodes.com/",'_blank');
+}
+
+
+
